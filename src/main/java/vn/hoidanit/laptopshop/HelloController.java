0cd73aa3a1727e45ded6b64201085aff2794fb22
QuocHuyGIT103 <quochuyab2003@gmail.com>
2024-12-17 23:58:01 +0700

    update pom.xml and security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,17 @@ public class HelloController {
 
     @GetMapping("/")
 	public String index() {
-		return "Hello World with Vu Quoc Huy";
+		return "Hello World Vu quoc Huy";
+	}
+
+	@GetMapping("/user")
+	public String usePage() {
+		return "only user can access this page";
+	}
+
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "only admin can access this page";
 	}
 
 }
