17a6c6286ec66328673a64a9df0cfc57e9fb1110
QuocHuyGIT103 <quochuyab2003@gmail.com>
2024-12-29 20:00:46 +0700

    done feature register

--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,7 +29,9 @@ public class Product {
 
     @NotNull
     @NotEmpty(message = "detailDesc không được để trống")
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String detailDesc;
+
 
     @NotNull
     @NotEmpty(message = "shortDesc không được để trống")
